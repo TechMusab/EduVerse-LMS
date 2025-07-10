f6fd1c8cdd57ec43453c95c83b59ada6f1ca7091
Musab <joiya4636@gmail.com>
2025-07-10 15:02:14 -0700

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -3,28 +3,19 @@ import User from '../models/User.js';
 import dotenv from 'dotenv';
 dotenv.config();
 const auth= (req, res, next) => {
-  //get token
     let token = req.headers['authorization'];
-    console.log(token);
-    // Support 'Bearer <token>' and plain token
-    if (token && token.startsWith('Bearer ')) {
-        token = token.split(' ')[1];
-    }
-    //check if token exists
     if (!token) {
         res.status(401).json({
             message: 'No token provided, authorization denied.'
         })
         return;
     }
-    //verify token
     try{
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         req.user = decoded;
         next();
     }
     catch(err) {
-        console.log(err);
         res.status(401).json({
             message: 'Token is not valid.'
         })
